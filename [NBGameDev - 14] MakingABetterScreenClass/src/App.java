eff583929be88eb6ec2bd5250b6e4fc2e98889da
Styles <cp.codyprice@gmail.com>
2014-11-11 20:56:24 -0600

    2056-11.10.2014: Ariel, up to NBGameDev 23

--- [NBGameDev - 14] MakingABetterScreenClass/src/App.java
+++ [NBGameDev - 14] MakingABetterScreenClass/src/App.java
@@ -1,28 +1,46 @@
 import java.awt.DisplayMode;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 
 public class App {
 
-	private Screen screen;
+	private ScreenManager screen;
 	private Image background;
 	private Animation animation;
 
+	private static final DisplayMode modes[] = {
+			new DisplayMode(1920, 1080, 32, DisplayMode.REFRESH_RATE_UNKNOWN),
+			//new DisplayMode(1920, 1080, 24, DisplayMode.REFRESH_RATE_UNKNOWN),
+			//new DisplayMode(1920, 1080, 16, DisplayMode.REFRESH_RATE_UNKNOWN),
+			new DisplayMode(800, 600, 32, DisplayMode.REFRESH_RATE_UNKNOWN),
+			new DisplayMode(800, 600, 24, DisplayMode.REFRESH_RATE_UNKNOWN),
+			new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN) };
+
 	public static void main(String[] args) {
-		DisplayMode displayMode = new DisplayMode(1920, 1080, 32,
-				DisplayMode.REFRESH_RATE_UNKNOWN);
 		App app = new App();
-		app.run(displayMode);
+		app.run();
 	}
 
-	public void run(DisplayMode displayMode) {
-		screen = new Screen();
+	public void loadImages() {
+		background = new ImageIcon("background.png").getImage();
+		Image tri1 = new ImageIcon("triangle1.png").getImage();
+		Image tri2 = new ImageIcon("triangle2.png").getImage();
+
+		animation = new Animation();
+		animation.addScene(tri1, 250);
+		animation.addScene(tri2, 250);
+	}
+
+	public void run() {
+		screen = new ScreenManager();
+
 		try {
-			screen.setFullScreen(displayMode, new JFrame());
-			loadPics();
+			DisplayMode displayMode = screen.findFirstCompatibleMode(modes);
+			screen.setFullScreen(displayMode);
+			loadImages();
 			movieLoop();
 		} finally {
 			screen.restoreScreen();
@@ -37,29 +55,23 @@ public class App {
 			long timePassed = System.currentTimeMillis() - cummulativeTime;
 			cummulativeTime += timePassed;
 			animation.update(timePassed);
-			
-			Graphics graphics = screen.getFullScreenWindow().getGraphics();
+
+			// draw and update screen
+			Graphics2D graphics = screen.getGraphics();
 			draw(graphics);
 			graphics.dispose();
-			
+			screen.update();
+
 			try {
 				Thread.sleep(20);
-			} catch(Exception ex) {}
+			} catch (Exception ex) {
+			}
 		}
-		
+
 	}
 
 	public void draw(Graphics graphics) {
-		graphics.drawImage(background, 0, 0, null);
+		graphics.drawImage(background, 0, 0, screen.getWidth(), screen.getHeight(), null);
 		graphics.drawImage(animation.getImage(), 0, 0, null);
-	}
-
-	public void loadPics() {
-		background = new ImageIcon("background.png").getImage();
-		Image tri1 = new ImageIcon("triangle1.png").getImage();
-		Image tri2 = new ImageIcon("triangle2.png").getImage();
-		animation = new Animation();
-		animation.addScene(tri1, 250);
-		animation.addScene(tri2, 250);
 	}
 }
