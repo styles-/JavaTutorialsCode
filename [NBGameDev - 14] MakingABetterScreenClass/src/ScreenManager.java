375d5f0c5f7efc7192cedd3fe971cb197c59bb0a
Styles <cp.codyprice@gmail.com>
2014-11-11 19:26:04 -0600

    1926-11.10.2014: commit NBGameDev up to video 20

--- [NBGameDev - 14] MakingABetterScreenClass/src/ScreenManager.java
+++ [NBGameDev - 14] MakingABetterScreenClass/src/ScreenManager.java
@@ -1,6 +1,13 @@
 import java.awt.DisplayMode;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Window;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
 
 public class ScreenManager {
 
@@ -30,6 +37,7 @@ public class ScreenManager {
 				}
 			}
 		}
+		return null;
 	}
 
 	// get current display mode
@@ -37,8 +45,113 @@ public class ScreenManager {
 		return videoCard.getDisplayMode();
 	}
 
-	public boolean displayModesMatch(DisplayMode displayMode,
-			DisplayMode displayMode2) {
-		return false;
+	public boolean displayModesMatch(DisplayMode mode, DisplayMode mode2) {
+		if (mode.getWidth() != mode2.getWidth()
+				|| mode.getHeight() != mode2.getHeight()) {
+			return false;
+		}
+
+		if (mode.getBitDepth() != DisplayMode.BIT_DEPTH_MULTI
+				&& mode2.getBitDepth() != DisplayMode.BIT_DEPTH_MULTI
+				&& mode.getBitDepth() != mode2.getBitDepth()) {
+			return false;
+		}
+
+		if (mode.getRefreshRate() != DisplayMode.REFRESH_RATE_UNKNOWN
+				&& mode2.getRefreshRate() != DisplayMode.REFRESH_RATE_UNKNOWN
+				&& mode.getRefreshRate() != mode2.getRefreshRate()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	// make frame full screen
+	public void setFullScreen(DisplayMode displayMode) {
+		JFrame frame = new JFrame();
+		frame.setUndecorated(true);
+		frame.setIgnoreRepaint(true);
+		frame.setResizable(false);
+		videoCard.setFullScreenWindow(frame);
+
+		if (displayMode != null && videoCard.isDisplayChangeSupported()) {
+			try {
+				videoCard.setDisplayMode(displayMode);
+			} catch (Exception ex) {
+			}
+		}
+
+		frame.createBufferStrategy(2);
+	}
+
+	// we will set Graphics object = to whatever this returns
+	public Graphics2D getGraphics() {
+		Window window = videoCard.getFullScreenWindow();
+
+		if (window != null) {
+			BufferStrategy strategy = window.getBufferStrategy();
+			return (Graphics2D) strategy.getDrawGraphics();
+		} else
+			return null;
+	}
+	
+	// updates display
+	public void update() {
+		Window window = videoCard.getFullScreenWindow();
+		
+		if (window != null) {
+			BufferStrategy strategy = window.getBufferStrategy();
+			
+			if (!strategy.contentsLost()) {
+				strategy.show();
+			}
+		}
+	}
+	
+	// returns full screen window
+	public Window getFullScreenWindow() {
+		return videoCard.getFullScreenWindow();
+	}
+	
+	// get width of window
+	public int getWidth() {
+		Window window = videoCard.getFullScreenWindow();
+		
+		if (window != null) {
+			return window.getWidth();
+		} else
+			return 0;
+	}
+	
+	// get height of window
+	public int getHeight() {
+		Window window = videoCard.getFullScreenWindow();
+		
+		if (window != null) {
+			return window.getHeight();
+		} else
+			return 0;
+	}
+	
+	// get out of fullscreen
+	public void restoreScreen() {
+		Window window = videoCard.getFullScreenWindow();
+		
+		if (window != null) {
+			window.dispose();
+		}
+		
+		videoCard.setFullScreenWindow(null);
+	}
+	
+	// create image compatible with monitor
+	public BufferedImage createCompatibleImage(int width, int height, int transparency) {
+		Window window = videoCard.getFullScreenWindow();
+		
+		if (window != null) {
+			GraphicsConfiguration gc = window.getGraphicsConfiguration();
+			return gc.createCompatibleImage(width, height, transparency);
+		} else
+			return null;
 	}
 }
